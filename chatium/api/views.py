ece79f307738a9df3f8ea6771be30a53e4265776
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2022-07-11 11:12:34 +0530

    worked on api side
    - made route configurations
    - made a sample rest api route for checking
    - migrated changes

--- chatium/api/views.py
+++ chatium/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from curses.ascii import HT
+from django.http import JsonResponse, HttpResponse
+from rest_framework.decorators import api_view
+
+from .models import * 
+from .serializers import * 
 
 # Create your views here.
+@api_view(['GET'])
+def index(request): 
+    return JsonResponse({
+        "status": "success"
+    })
