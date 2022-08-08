917f0a3247e38468440fd55b034592b732bbe822
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2022-08-08 19:00:08 +0530

    redesigned menu bar ✌️

--- chatium/frontend/src/components/menuBar.jsx
+++ chatium/frontend/src/components/menuBar.jsx
@@ -1,26 +1,26 @@
-import React from 'react'; 
-import { deepPurple } from '@mui/material/colors';
-import Avatar from '@mui/material/Avatar';
-import TimelapseRoundedIcon from '@mui/icons-material/TimelapseRounded';
-import AddReactionRoundedIcon from '@mui/icons-material/AddReactionRounded';
-import ExpandMoreRoundedIcon from '@mui/icons-material/ExpandMoreRounded';
+import React from "react";
+import { deepPurple } from "@mui/material/colors";
+import Avatar from "@mui/material/Avatar";
+import TimelapseRoundedIcon from "@mui/icons-material/TimelapseRounded";
+import AddReactionRoundedIcon from "@mui/icons-material/AddReactionRounded";
+import ExpandMoreRoundedIcon from "@mui/icons-material/ExpandMoreRounded";
 
-import "./menuBar.css"; 
+import "./menuBar.css";
 
 function MenuBar() {
-   return (
-      <div className="menu-wrapper">
-         <div classname="left">
-            <img src="static/chatium-logo.png" className="logo-centered" />
-         </div>
-         <div className="right">
-            <TimelapseRoundedIcon style={{ color: "#c5dff2c9" }} fontSize="medium" />
-            <AddReactionRoundedIcon style={{ color: "#c5dff2c9" }} fontSize="medium" />
-            <Avatar sx={{ bgcolor: deepPurple[500] }}>LD</Avatar>           
-            <ExpandMoreRoundedIcon style={{ color: "#c5dff2c9" }} fontSize="medium" />
-         </div>
-      </div> 
-   ); 
+  return (
+    <div className="nav-wrapper">
+      <div className="nav-left">
+        <img src="static/chatium-logo-cropped.png" alt="logo" id="logo" />
+      </div>
+      <div className="nav-right" fontSize="small" style={{ color: "#9FB3CA" }}>
+        <TimelapseRoundedIcon id="status-icon" />
+        <AddReactionRoundedIcon />
+        <div className="profile-pic-icon"></div>
+        <ExpandMoreRoundedIcon id="expand-more-btn" />
+      </div>
+    </div>
+  );
 }
 
-export default MenuBar; 
+export default MenuBar;
